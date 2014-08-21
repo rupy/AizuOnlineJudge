51ba736e885886b27505ad0a47da39455e0ce1c7
rupy <rupyapps@gmail.com>
2014-08-22 01:45:36 +0900

    リファクタリング

--- vol0/0012a_point_in_a_triangle.rb
+++ vol0/0012a_point_in_a_triangle.rb
@@ -4,7 +4,7 @@ def outer_product(vec1, vec2)
 end
 
 # ベクトルの差
-def diff(vec1, vec2)
+def subtract(vec1, vec2)
   result = {}
   result['x'] = vec1['x'] - vec2['x']
   result['y'] = vec1['y'] - vec2['y']
@@ -18,29 +18,27 @@ def a_point_in_a_triangle
   points = []
   3.times do |i|
     point = {}
-    point['x'] = input[i*2]
-    point['y'] = input[i*2+1]
+    point['x'], point['y'] = input[i * 2], input[i * 2 + 1]
     points.push point
   end
   p_ = {}
-  p_['x'] = input[6]
-  p_['y'] = input[7]
-  p points
-  p p_
+  p_['x'], p_['y'] = input[6], input[7]
+  # p points
+  # p p_
 
-  outer = []
   # output
+  outer = []
   3.times do |i|
     # 三角形の頂点A, B, Cに対して
     # ある点Pが存在するときに
     # ベクトルAB,BC,CAとAP,BP,CPを使って
     # ABxAP, BCxBP, CAxCP （xは外積）
     # を計算し、全て符号が一致すれば点Pは三角形の内部にある
-    vec1 = diff points[i], points[(i + 1) % 3] # 頂点から頂点のベクトル
-    vec2 = diff points[i], p_ # 頂点から点Pへのベクトル
+    vec1 = subtract points[i], points[(i + 1) % 3] # 頂点から頂点のベクトル
+    vec2 = subtract points[i], p_ # 頂点から点Pへのベクトル
     outer.push outer_product vec1, vec2
   end
-  puts (outer.all?{|o|o>0} || outer.all?{|o|o<0}) ? 'YES' : 'NO'
+  puts (outer.all?{|o|o > 0} || outer.all?{|o|o < 0}) ? 'YES' : 'NO'
 end
 
 a_point_in_a_triangle
