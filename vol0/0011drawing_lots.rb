47ae3c4b6f2dd18df34f9d70d1ea793cfd7db2f6
rupy <rupyapps@gmail.com>
2014-08-22 00:11:20 +0900

    結果を出力

--- vol0/0011drawing_lots.rb
+++ vol0/0011drawing_lots.rb
@@ -1,14 +1,30 @@
 def drawing_lots
 
   swap_list = []
+
+  # 入力
   w = gets.to_i
   n = gets.to_i
   n.times do
     swp = gets.split(',').map{|i|i.to_i}
     swap_list.push swp
   end
-  p swap_list
-end
 
+# 計算と出力
+  w.times do |i|
+    current_line = i+1
+    # 下からたどるためにreverseする
+    swap_list.reverse.each do |swp|
+      if swp.include? current_line
+        if swp[0] == current_line
+          current_line = swp[1]
+        else
+          current_line = swp[0]
+        end
+      end
+    end
+    puts current_line
+  end
+end
 
 drawing_lots
