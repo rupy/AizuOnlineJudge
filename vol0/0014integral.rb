009020c6b71ded0b26818975b7e6929d8c658aab
rupy <rupyapps@gmail.com>
2014-08-29 00:24:35 +0900

    injectを使って書き換え

--- vol0/0014integral.rb
+++ vol0/0014integral.rb
@@ -1,15 +1,8 @@
 def integral
 
   d = gets.to_i
-  area = 0
-#  0.step(599, d) do |s|
-  (0...600).step(d) do |x|
-    area += d * (x ** 2)
-    puts x
-  end
-  puts area
-
-
+  result = (0...600).step(d).inject{|area, x| area += d * (x ** 2)}
+  puts result
 
 end
 
